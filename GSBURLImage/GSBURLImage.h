8246e216499bbb0e4b47af5a3093cb37e2cec7ac
Gareth Bestor <bestor@gareths-macbook-pro.fritz.box>
2016-06-02 11:51:03 +1200

    Initial Commit

---
+++ GSBURLImage/GSBURLImage.h
@@ -0,0 +1,13 @@
+//
+//  GSBURLImage.h
+//  GSBURLImage
+//
+//  Created by Gareth Bestor on 2/06/16.
+//  Copyright © 2016 Xiphware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSBURLImage : NSObject
+
+@end
---
+++ GSBURLImage/GSBURLImage.m
@@ -0,0 +1,13 @@
+//
+//  GSBURLImage.m
+//  GSBURLImage
+//
+//  Created by Gareth Bestor on 2/06/16.
+//  Copyright © 2016 Xiphware. All rights reserved.
+//
+
+#import "GSBURLImage.h"
+
+@implementation GSBURLImage
+
+@end
